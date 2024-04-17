1713026e721ce963f36d0fddff4a63448fa08194
rickwysocki <16529152+rickwysocki@users.noreply.github.com>
2024-04-17 19:21:41 -0400

    Add inactive form

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -6,11 +6,19 @@ export default async function Tasks(){
 
   return (
     <div>
-        <h2 className="text-xl">Tasks</h2>
+
+        <h2 className="text-xl">New Tasks</h2>
+        <form className="flex flex-row gap-4">
+            <label>New Task</label>
+            <input className="bg-neutral-200"></input>
+            <button>Submit</button>
+        </form>
+
+        <h2 className="text-xl">Current Tasks</h2>
       {rows.map((row) => (
         <div key={row.taskid}>
             <input type="checkbox" id="{row.taskid}" name="{row.taskid}" className="mr-4" />
-            <label for="{row.taskid}">{row.taskdescription}</label>
+            <label htmlFor="{row.taskid}">{row.taskdescription}</label>
         </div>
       ))}
     </div>
