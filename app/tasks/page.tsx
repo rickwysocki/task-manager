5c39eb7ed1971476d4cbca0bc761e020ea3864df
rickwysocki <16529152+rickwysocki@users.noreply.github.com>
2024-04-17 19:12:36 -0400

    Add inactive checkboxes

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -8,8 +8,9 @@ export default async function Tasks(){
     <div>
         <h2 className="text-xl">Tasks</h2>
       {rows.map((row) => (
-        <div key={row.id}>
-          {row.taskid} - {row.taskdescription}
+        <div key={row.taskid}>
+            <input type="checkbox" id="{row.taskid}" name="{row.taskid}" className="mr-4" />
+            <label for="{row.taskid}">{row.taskdescription}</label>
         </div>
       ))}
     </div>
